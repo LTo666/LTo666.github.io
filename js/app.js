1d73e099330ebcdfa8cd5c11017b4f0bfb222145
Bruce Luk <lukusto@gmail.com>
2022-06-21 03:54:35 +0800

    mod card layout and add intro text

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
 $(function () {
-  wow = new WOW(
-    {
-    boxClass:     'wow',      // default
-    animateClass: 'animated', // default
-    offset:       200,          // default
-    mobile:       true,       // default
-    live:         false        // default
-  }
-  )
-  wow.init();
-  const scrollFunction = () => {
+  wow = new WOW({
+    boxClass: "wow",
+    animateClass: "animated",
+    offset: 100,
+    mobile: true,
+    live: false,
+  })
+
+  wow.init()
+
+  const chNavbar = () => {
     if (
       document.body.scrollTop > 54 ||
       document.documentElement.scrollTop > 54
     ) {
       $("#header").removeClass("bg-primary text-primary-content pt-4")
       $("#header").addClass("bg-neutral text-neutral-content")
-      $("#navbox").removeClass("bg-primary text-primary-content shadow-neutral-600 w-80")
-      $("#navbox").addClass("bg-neutral text-neutral-content shadow-neutral-900 w-60")
+      $("#navbox").removeClass(
+        "bg-primary text-primary-content shadow-neutral-600 w-80"
+      )
+      $("#navbox").addClass(
+        "bg-neutral text-neutral-content shadow-neutral-900 w-60"
+      )
       $(".navbar-start > a").removeClass("text-3xl btn-lg")
       $(".navbar-start > a").addClass("text-2xl")
       $("header label").removeClass("btn-lg")
@@ -28,8 +32,12 @@ $(function () {
     } else {
       $("#header").addClass("bg-primary text-primary-content pt-4")
       $("#header").removeClass("bg-neutral text-neutral-content")
-      $("#navbox").addClass("bg-primary text-primary-content shadow-neutral-600 w-80")
-      $("#navbox").removeClass("bg-neutral text-neutral-content shadow-neutral-900 w-60")
+      $("#navbox").addClass(
+        "bg-primary text-primary-content shadow-neutral-600 w-80"
+      )
+      $("#navbox").removeClass(
+        "bg-neutral text-neutral-content shadow-neutral-900 w-60"
+      )
       $(".navbar-start > a").addClass("text-3xl btn-lg")
       $(".navbar-start > a").removeClass("text-2xl")
       $("header label").addClass("btn-lg")
@@ -41,7 +49,11 @@ $(function () {
   }
 
   window.onscroll = () => {
-    scrollFunction()
+    chNavbar()
   }
 
+  $('#send-btn').click((e) => {
+    $('.form-control input').val("")
+    $('.form-control textarea').val("")
+  })
 })
