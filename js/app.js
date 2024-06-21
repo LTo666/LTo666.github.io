ea2bfcfc5443e0a26428faee712f38e1fa4b6dbd
Bruce Luk <lukusto@gmail.com>
2024-06-22 00:48:16 +0800

    Fix

--- js/app.js
+++ js/app.js
@@ -77,41 +77,50 @@ $(function () {
   const maxWidth = $("#intro > div.wrapper > div").width();
   $("#header").width(maxWidth);
 
-  $("#intro > div.wrapper > div").on("resize", function () {
+  $(window).on("resize", function () {
     // Code to execute when the element is resized
     const newWidth = $("#intro > div.wrapper > div").width();
+    console.log(newWidth);
     $("#header").width(newWidth);
   });
   /** Responsive navbar */
 
+  const topHeight = $("#headerContainer").height();
+  console.log(topHeight);
+  $("#intro").css("margin-top", topHeight);
+
   const chNavbar = () => {
     if (document.body.scrollTop > 54 || document.documentElement.scrollTop > 54) {
+      $("body").removeClass("bg-primary");
+      $("body").addClass("bg-neutral");
       $("#headerContainer").removeClass("bg-primary");
       $("#headerContainer").addClass("bg-neutral");
       $("#header").removeClass("bg-primary text-primary-content pt-4");
       $("#header").addClass("bg-neutral text-neutral-content");
       $("#navbox").removeClass("bg-primary text-primary-content shadow-neutral-600 w-80");
       $("#navbox").addClass("bg-neutral text-neutral-content shadow-neutral-900 w-60");
-      $(".navbar-start > a").removeClass("text-3xl btn-lg");
-      $(".navbar-start > a").addClass("text-2xl");
+      // $("#header > div > a").removeClass("text-3xl btn-lg");
+      // $("#header > div > a").addClass("text-2xl");
       $("header label").removeClass("btn-lg");
       $("header path").attr("stroke-width", "2");
       $("header path").attr("d", "M4 6h16M4 12h16M4 18h16");
-      $("#nav-link a").removeClass("text-2xl");
+      $("#nav-link a").removeClass("text-xl");
       $("#nav-link a").addClass("text-lg");
     } else {
+      $("body").addClass("bg-primary");
+      $("body").removeClass("bg-neutral");
       $("#headerContainer").addClass("bg-primary");
       $("#headerContainer").removeClass("bg-neutral");
       $("#header").addClass("bg-primary text-primary-content pt-4");
       $("#header").removeClass("bg-neutral text-neutral-content");
       $("#navbox").addClass("bg-primary text-primary-content shadow-neutral-600 w-80");
       $("#navbox").removeClass("bg-neutral text-neutral-content shadow-neutral-900 w-60");
-      $(".navbar-start > a").addClass("text-3xl btn-lg");
-      $(".navbar-start > a").removeClass("text-2xl");
+      // $("#header > div > a").addClass("text-3xl btn-lg");
+      // $("#header > div > a").removeClass("text-2xl");
       $("header label").addClass("btn-lg");
       $("header path").attr("stroke-width", "4");
       $("header path").attr("d", "M4 6h16M4 12h8m-8 6h16");
-      $("#nav-link a").addClass("text-2xl");
+      $("#nav-link a").addClass("text-xl");
       $("#nav-link a").removeClass("text-lg");
     }
   };
